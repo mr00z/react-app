6546c0cfe0bed8fda8d2252e0dd2ce1e351496d9
Marcin Mróz <mrfrostn@gmail.com>
2020-04-04 19:42:29 +0200

    Added youtube video component

--- src/features/SongForm/components/SongFormResult.js
+++ src/features/SongForm/components/SongFormResult.js
@@ -2,19 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './songForm.scss';
+import YouTube from 'react-youtube';
 
-const SongFormResult = ({ name, author }) => {
-  const hasSong = name && author;
+const SongFormResult = ({ song }) => {
   return (
     <div
       data-testid="song-form-result"
       // eslint-disable-next-line max-len
       className={`has-text-centered has-text-light is-size-5-mobile is-size-4-tablet is-size-3-desktop is-size-4-widescreen ${styles.songForm__result}`}
     >
-      {hasSong ? (
+      {song ? (
         <>
           Your song is: <br />
-          {name} by {author}
+          {song.title} by {song.author}
+          {song?.servicesData?.youtube?.responseData && (
+            <YouTube
+              videoId={song.servicesData.youtube.responseData.id.videoId}
+              containerClassName={styles.songForm__result_video_container}
+              className={styles.songForm__result_video}
+            />
+          )}
         </>
       ) : (
         <>No songs matching your criteria</>
@@ -24,8 +31,13 @@ const SongFormResult = ({ name, author }) => {
 };
 
 SongFormResult.propTypes = {
-  name: PropTypes.string,
-  author: PropTypes.string
+  song: PropTypes.shape({
+    author: PropTypes.string,
+    title: PropTypes.string,
+    genres: PropTypes.array,
+    moods: PropTypes.array,
+    servicesData: PropTypes.object
+  })
 };
 
 export default SongFormResult;
