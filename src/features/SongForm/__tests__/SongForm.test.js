2e798090e9f3ff54882c746dc537016ea2e787ac
Marcin Mróz <mrfrostn@gmail.com>
2020-04-11 19:26:43 +0200

    Fixed tests

--- src/features/SongForm/__tests__/SongForm.test.js
+++ src/features/SongForm/__tests__/SongForm.test.js
@@ -1,43 +1,30 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import SongForm from '../components/SongForm';
 
-it('should render all moods available', async () => {
+it('displays list of moods and gives a result', async () => {
   const allMoods = ['Test_Mood1', 'Test_Mood2', 'Test_Mood3'];
 
   global.fetch = jest.fn().mockImplementation(() =>
     Promise.resolve({
-      json: () => Promise.resolve(allMoods)
-    })
-  );
-
-  const { findByRole } = render(<SongForm />);
-
-  const dropdownNode = await findByRole('combobox');
-
-  const dropdownOptions = Array.apply(null, dropdownNode.options).map(
-    option => option.text
-  );
-
-  expect(dropdownOptions).toContain('Test_Mood1');
-  expect(dropdownOptions).toContain('Test_Mood2');
-  expect(dropdownOptions).toContain('Test_Mood3');
-
-  global.fetch.mockReset();
-});
-
-it('should render a result', async () => {
-  global.fetch = jest.fn().mockImplementation(() =>
-    Promise.resolve({
-      json: () => Promise.resolve([])
+      json: () => Promise.resolve(allMoods),
     })
   );
 
   const { getByText, findByTestId } = render(<SongForm />);
 
+  const indicator = document.querySelector('div[class$=indicatorContainer]');
+  fireEvent.click(indicator);
+
+  await waitFor(() => document.querySelector('div[class$=MenuList]'));
+
+  const options = document.querySelector('div[class$=MenuList]');
+
+  expect(options.childElementCount).toEqual(allMoods.length);
+
+  fireEvent.click(options.firstChild);
+
   fireEvent.click(getByText('Give me the song!'));
 
   expect(await findByTestId('song-form-result')).toBeDefined();
-
-  global.fetch.mockReset();
 });
