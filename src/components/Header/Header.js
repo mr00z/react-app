f1723f95c17e99ce23a56671a44203c7856a3422
Marcin Mróz <mrfrostn@gmail.com>
2020-04-14 17:09:03 +0200

    Added link to main page in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import genie from '../../assets/genie.png';
 
 import './header.scss';
 
-const Header = () => (
-  <div className="is-flex header__container">
-    <img src={genie} className="header__img" />
-    <div className="has-text-centered header__text">Music Jinn</div>
-  </div>
-);
-
+const Header = () => {
+  const history = useHistory();
+  return (
+    <div className="is-flex header__container" onClick={() => history.push('/')}>
+      <img src={genie} className="header__img" />
+      <div className="has-text-centered header__text">Music Jinn</div>
+    </div>
+  );
+};
 export default Header;
