b369a77980108f7dabef8c4fcf15b88c1e1052b8
Marcin Mróz <mrfrostn@gmail.com>
2020-05-05 18:11:26 +0200

    Added api key and icon

--- src/integrations/MusicJinnAPIConnector.js
+++ src/integrations/MusicJinnAPIConnector.js
@@ -2,7 +2,11 @@ class MusicJinnAPIConnector {
   static endpoint = process.env.MUSIC_JINN_API_URL;
 
   static get(requestUrl) {
-    return fetch(`${this.endpoint}/${requestUrl}`).then((response) => {
+    return fetch(`${this.endpoint}/${requestUrl}`, {
+      headers: {
+        'X-Api-Key': process.env.MUSIC_JINN_API_KEY,
+      },
+    }).then((response) => {
       if (response.status === 204) return null;
 
       return response.json();
