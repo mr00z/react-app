59da40e4b074f748ef1ee20c7c47b218f84666fd
Marcin Mróz <mrfrostn@gmail.com>
2020-05-14 20:04:27 +0200

    Added back react-youtube [iOS debug]

--- src/features/SongForm/components/SongForm.js
+++ src/features/SongForm/components/SongForm.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import FormField from '../../../components/Form/FormField';
 import SubmitButton from '../../../components/Form/SubmitButton';
@@ -118,7 +117,7 @@ const SongForm = () => {
           </SubmitButton>
         </FormField>
       </Form>
-      {/* {songReady && <SongFormResult song={song} />} */}
+      {songReady && <SongFormResult song={song} />}
     </>
   );
 };
