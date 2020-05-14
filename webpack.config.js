a02ff51979f681fc3adb27008c9271be7a955dfe
Marcin Mróz <mrfrostn@gmail.com>
2020-05-14 18:36:26 +0200

    Removed _redirects

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const path = require('path');
 const devMode = process.env.NODE_ENV !== 'production';
@@ -99,7 +98,6 @@ module.exports = {
       systemvars: true,
       silent: true,
     }),
-    new CopyPlugin([{ from: 'public/_redirects', to: './' }]),
   ],
   devServer: {
     contentBase: path.resolve(__dirname, 'public/assets'),
